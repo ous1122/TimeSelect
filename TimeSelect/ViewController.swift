f2a7209d67b9e48449ac6340880867b42b934251
D7703_13 <d7703_13@d7703-13ui-imac.local>
2018-04-11 16:11:52 +0900

    123

--- TimeSelect/ViewController.swift
+++ TimeSelect/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer = Timer()
+    
+    @IBOutlet weak var CTime: UILabel!
+    @IBOutlet weak var PTime: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(timer) in self.updateTime()})
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func ChangePick(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "YYYY년 MM월 dd일 HH : mm : ss a EEE"
+        PTime.text = formatter.string(from: datePickerView.date)
+        
+    }
+    
+    @objc func updateTime() {
+        let date1 = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy-MM-dd a HH:mm:ss EEE"
+        let time1 = formatter.string(from: date1)
+        CTime.text = time1
     }
 
-
 }
-
